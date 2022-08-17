60f88834d360f93612c88965694a2dc57e16893d
ADB <arnauddbu@gmail.com>
2022-08-17 23:14:26 +0200

    mobile-dev

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,8 @@
-const TextBlock = require("./src/_includes/components/TextBlock");
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy({
     "src/assets/": "",
   });
-
-  eleventyConfig.addShortcode("TextBlock", TextBlock);
 
   return {
     dir: {
